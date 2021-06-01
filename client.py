925f0965d29483cc7bca1d1d16d8d2e1efbe40aa
Edgardo Reinoso <e.reinoso@azerion.com>
2021-06-01 23:40:49 +0200

    solving the issue of multiple listeners v1

--- client.py
+++ client.py
@@ -18,8 +18,10 @@ table = dynamodb.Table(os.environ['ddbTable'])
 # validating string
 regex_string = '^[a-zA-Z]{3,}$'  # would probably have to change
 
+
 def isValidString(string):
     return re.match(regex_string, string)
+
 
 def build_process(port_n, name, parsedTime):
     build = codebuild_client.start_build(
@@ -53,9 +55,10 @@ def build_process(port_n, name, parsedTime):
     )
     return build
 
+
 def readingFromDDBTable(name):
     # query dynamodb table for the item
-    logger.info("Reading from table")
+    logger.info("Checking if client already exists")
     res = ddb_client.query(
         TableName=os.environ['ddbTable'],
         KeyConditionExpression='#Client = :Client',
@@ -70,79 +73,48 @@ def readingFromDDBTable(name):
     )
     return res
 
+
 @app.route("/createclient", methods=['POST'])
 def createclient():
     # time
     currentTime = ""
     currentTime = datetime.utcnow()
     parsedTime = str(currentTime.strftime("%Y-%m-%dT%H:%M:%SZ"))
-    areThereItems = False
-    name=request.json.get('name')
-    port_n=request.json.get('port')
-    
+    ddbQuery = {}
+    name = request.json.get('name')
+    port_n = request.json.get('port')
+
     # should probably return an error if a certain string is not within
-    # certain parameters 
+    # certain parameters
     if isValidString(name):
+        print(name)
         ddbQuery = readingFromDDBTable(name)
         if (ddbQuery['Count'] == 0):
-            areThereItems = True
-        
-        print(areThereItems)
-        # logger.info("Starting Build process")
-        # build = build_process(port_n, name, parsedTime)
+            logger.info("Starting Build process")
+            build = build_process(port_n, name, parsedTime)
 
-        # try:
-        #     # Putting item in dynamodb table
-        #     logger.info("Putting dynamodb item")
-        #     hello = table.put_item(
-        #             Item={
-        #                 'Client': name,
-        #                 'Date': parsedTime,
-        #                 'Port': port_n,
-        #                 'BuildId': '',
-        #                 'ListenerArn': '',
-        #                 'RuleArn': '',
-        #                 'TargetArn': '',
-        #                 'TaskDefinitionArn': '',
-        #                 'SecurityGroupId': ''
-        #             },
-        #             ConditionExpression='attribute_not_exists(Client)',
-        #         )
-        #     print('response from ddb: ', hello)
-
-        #     # Starting the build id
-        #     logger.info("Starting Build process")
-        #     build = build_process(port_n, name, parsedTime)
-            
-        #     print('this is name: ',name)
-
-        #     # Updating the BuildId
-        #     table.update_item(
-        #         TableName='frontend-ddb-client',
-        #         Key={
-        #             'Client': {
-        #                 'S': name,
-        #             }
-        #         },
-        #         # UpdateExpression="SET ListenerArn = :LArn, TargetArn = :TArn, SecurityGroupId = :SGId",
-        #         UpdateExpression="SET BuildId = :BId",
-        #         ExpressionAttributeValues={
-        #             ':BId': {
-        #                 'S': build['build']['id']
-        #             },
-        #         }
-        #     )
-        # except botocore.exceptions.ClientError as error:
-            # if error.response['Error']['Code'] == 'ConditionalCheckFailedException':
-            #     res = Response("Client already exists",
-            #                 status=400, mimetype='application/json')
-            #     return res
-            # else:
-            #     raise error
-        
-        res = Response("Client added successfully",
-                       status=200, mimetype='application/json')
-        return res
+            # Putting item in dynamodb table
+            logger.info("Putting dynamodb item")
+            table.put_item(
+                Item={
+                    'Client': name,
+                    'Date': parsedTime,
+                    'Port': port_n,
+                    'BuildId': build['build']['id'],
+                    'ListenerArn': '',
+                    'RuleArn': '',
+                    'TargetArn': '',
+                    'TaskDefinitionArn': '',
+                    'SecurityGroupId': ''
+                },
+            )
+            res = Response("Client added successfully",
+                            status=200, mimetype='application/json')
+            return res
+        else:
+            res = Response("Client already exists",
+                            status=400, mimetype='application/json')
+            return res
     else:
         res = Response("Please enter a valid string",
                        status=400, mimetype='application/json')
@@ -153,7 +125,7 @@ def createclient():
 
 @app.route("/removeclient", methods=['DELETE'])
 def removeclient():
-    name=request.json.get('name') # client name
+    name = request.json.get('name')  # client name
 
     # there should be a handler here to check whether the item
     # is in the db
@@ -174,7 +146,7 @@ def removeclient():
             if error.response['Error']['Code'] == 'ConditionalCheckFailedException':
                 # might have to check whether this is possible
                 res = Response("Client was not found",
-                            status=400, mimetype='application/json')
+                               status=400, mimetype='application/json')
                 return res
             else:
                 raise error
