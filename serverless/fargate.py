7fcf7f99b038db4e320cd2be484b88a9e10fb0ef
Ed Reinoso <edgardojesus16@gmail.com>
2022-06-30 00:01:37 +0200

    updated fargate on/off
    tested the mock_lambda with eventbridge and changed the time to 24 hours

--- serverless/fargate.py
+++ serverless/fargate.py
@@ -7,31 +7,33 @@ ecs = boto3.client('ecs')
 
 currentTime = datetime.utcnow()
 
-# def handler(event, context):
-cluster = 'lazzaro-front-cluster-pre'
+def handler(event, context):
+    cluster = 'lazzaro-front-cluster-pre'
 
-# have to check couple of variables.
-# 1. whether eventbridge has events scheduled on gmt
-# 2. military time should be accounted
-# 3. invocation would work exactly with the specified logic
+    # have to check couple of variables.
+    # 1. whether eventbridge has events scheduled on gmt ✅
+    # 2. 24h should be accounted ✅
+    # 3. invocation would work exactly with the specified logic
 
-if currentTime.strftime("%I:%M:%S %p") == "8:00:00 AM":
-    desireCount = 1
-else:
-    desireCount = 0
+    if currentTime.strftime('%H:%M:%S') == "8:00:00":
+        # turn on services
+        desireCount = 1
+    else:
+        # turn off services
+        desireCount = 0
 
-response = ecs.list_services(
-    cluster=cluster,
-)
-
-for n in response['serviceArns']:
-    ecs.update_service(
+    response = ecs.list_services(
         cluster=cluster,
-        service=n,
-        desiredCount=desireCount,
     )
 
-# return {
-#     'statusCode': 200,
-#     'body': json.dumps('ECS Saving has been applied')
-# }
+    for n in response['serviceArns']:
+        ecs.update_service(
+            cluster=cluster,
+            service=n,
+            desiredCount=desireCount,
+        )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('ECS savings has been applied')
+    }
