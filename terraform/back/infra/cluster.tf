53529682c5a75154bb9fb0d81b4fcf38fb7cb3e6
Ed Reinoso <edgardojesus16@gmail.com>
2023-04-06 08:54:15 +0200

    updated ecs cluster capacity provider

--- terraform/back/infra/cluster.tf
+++ terraform/back/infra/cluster.tf
@@ -1,8 +1,25 @@
-resource "aws_ecs_cluster" "ecs-cluster" {
- name = "lazzaro-back-cluster-${terraform.workspace}"
- capacity_providers = ["FARGATE_SPOT", "FARGATE"]
+# DEPRECATED - DELETE SOON 🚧
 
- default_capacity_provider_strategy {
-   capacity_provider = "FARGATE"
- }
+# resource "aws_ecs_cluster" "ecs-cluster" {
+#  name = "lazzaro-back-cluster-${terraform.workspace}"
+#  capacity_providers = ["FARGATE_SPOT", "FARGATE"]
+
+#  default_capacity_provider_strategy {
+#    capacity_provider = "FARGATE"
+#  }
+# }
+
+
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "lazzaro-back-cluster-${terraform.workspace}"
+}
+
+resource "aws_ecs_cluster_capacity_providers" "ecs_cluster_capacity_provider" {
+  cluster_name = aws_ecs_cluster.ecs_cluster.name
+
+  capacity_providers = ["FARGATE_SPOT","FARGATE"]
+
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+  }
 }
